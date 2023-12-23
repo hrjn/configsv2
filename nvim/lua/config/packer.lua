fd2f4b39b82d5bcd1fc6def1b9bae10d54546f80
Harizo Rajaona <harizo.rajaona@gmail.com>
2023-12-24 00:05:24 +0100

    nvim: add mason and lsp-zero (WIP)

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -26,6 +26,23 @@ return require('packer').startup(function(use)
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
+    -- mason
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig"
+    }
+    -- lsp-zero
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 
 end)
 
